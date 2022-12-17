bdcd3ef6f4b20f42955ab8ee6a3b8474d5a2e95d
Alex <akaravashkin5@gmail.com>
2022-12-17 17:18:17 +0300

    Alt Shift F

--- exampl_43/Program (2).cs
+++ exampl_43/Program (2).cs
@@ -16,13 +16,13 @@ static double NumberPlus()
     return number;
 }
 
-// Метод записи массива справо - налево (честно стырен с семинара!!!)
+// Метод записи массива справа - налево (честно стырен с семинара!!!)
 int[] CopyReverseArray(int[] array)
 {
-    int size = array.Length; 
+    int size = array.Length;
     int[] result = new int[size]; // Копия - такой же размер
     int last = size - 1;
-    for (int i = 0; i < size; i++) // i++ - слево - направо
+    for (int i = 0; i < size; i++) // i++ - слева - направо
     {
         result[i] = array[last - i]; // Последний элемент - текущий
     }
@@ -31,20 +31,20 @@ int[] CopyReverseArray(int[] array)
 
 //Ввод числа в десятичном формате
 Console.WriteLine($"Введите число в десятичном формате : ");
-int dec = Convert.ToInt32 (NumberPlus());
+int dec = Convert.ToInt32(NumberPlus());
 
 
 //Получение массива остатков деления на 2
 int[] array1 = new int[1];
 int i = 0;
-while (dec>0)
+while (dec > 0)
 {
-    Array.Resize(ref array1, i+1);
-    array1[i] = dec%2;
-    dec /=2;
-    i++;  
+    Array.Resize(ref array1, i + 1);
+    array1[i] = dec % 2;
+    dec /= 2;
+    i++;
 }
 
 
-//Вывод массива
+//Вывод перевёрнутого массива
 Console.WriteLine($"Ваше число в двоичном формате: {string.Join("", CopyReverseArray(array1))}");
